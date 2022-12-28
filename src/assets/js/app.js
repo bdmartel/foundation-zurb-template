3b96e0c9a6f86a884f477af8d629244cd9f18b05
bdmartel <bdmartel@gmail.com>
2022-12-28 12:56:52 -0500

    before adding responsive-nav-social stickynav

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,9 @@ require('foundation-sites');
 
 
 $(document).foundation();
+
+// $('.title-bar').on('sticky.zf.stuckto:top', function(){
+//   $(this).addClass('shrink');
+// }).on('sticky.zf.unstuckfrom:top', function(){
+//   $(this).removeClass('shrink');
+// })
